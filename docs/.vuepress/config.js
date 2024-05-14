c447c858330e10edb694c9cf872b46822209c6e3
caiwanxia <caiwanxia@dm-ai.cn>
2024-05-14 17:29:21 +0800

    fix:修改vuepress配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  base: '/yuUI/',
+  title: 'Yu-UI',
+  base: '/yu-ui/',
   themeConfig: {
+    nav: [
+      {
+        text: 'Home', link: '/'
+      }, {
+        text: 'Github', link: 'https://github.com/fionaVg1/yu-ui'
+      },
+      {
+        text: 'Npm', link: 'https://www.npmjs.com/package/@fiona_cai/yu-ui'
+      }
+    ],
     sidebar: [
       '/',
       '/componentDocs/card'
